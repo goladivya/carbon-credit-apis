bab6b1a8209be826c036ed7e1ee3b0a757c657aa
goladivya <divyagola19@gmail.com>
2025-06-27 15:03:39 +0530

    added differnt vehicle option here

--- carbon-credit-calculator/src/requests/transportationCreationRequests.ts
+++ carbon-credit-calculator/src/requests/transportationCreationRequests.ts
@@ -2,7 +2,8 @@ import { CreditCreationRequest } from "./creditCreationRequest";
 import { TransportationConstant } from "../constants/transportationConsrants";
 
 export class TransportationCreationRequest implements CreditCreationRequest{
+    vehicleType!: string;
     fuelUsed!: number;         
-    fuelUnit!: string; 
+    fuelUnit!: "l"; 
     transportationconstants = new TransportationConstant();
 }
