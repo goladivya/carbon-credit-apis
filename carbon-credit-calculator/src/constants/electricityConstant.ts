c313b935b484dc11a34a9ccfac0c8dfebc3eedc5
goladivya <divyagola19@gmail.com>
2025-06-27 15:00:55 +0530

    fix the constants

--- carbon-credit-calculator/src/constants/electricityConstant.ts
+++ carbon-credit-calculator/src/constants/electricityConstant.ts
@@ -2,5 +2,5 @@ import { SubSectorConstants } from "./subSectorConstants";
 
 export class TransmissionConstants implements SubSectorConstants{
      gridEmissionFactor: number = 0.82;
-    gridEmissionFactorUnit: string = "tCO2/MWh";
+    gridEmissionFactorUnit: string = "tCO2/MWh";  
 }
